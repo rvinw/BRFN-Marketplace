b4f68c1f5e6f3dd07b381d876a7eeb749bb72e3a
rvinw <rvinw@icloud.com>
2026-02-18 16:36:39 +0000

    smth

--- backend/marketplace/views_api.py
+++ backend/marketplace/views_api.py
@@ -1,14 +1,21 @@
-from rest_framework import viewsets, permissions
+from rest_framework import permissions, viewsets
+
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by("name")
     serializer_class = CategorySerializer
     permission_classes = [permissions.AllowAny]
 
+
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.select_related("category", "producer").all().order_by("-created_at")
+    queryset = (
+        Product.objects.select_related("category", "producer")
+        .all()
+        .order_by("-created_at")
+    )
     serializer_class = ProductSerializer
     permission_classes = [permissions.AllowAny]
 
