cdfacbb09f6eeb6f852e5de20addbf06fed2ce3d
rvinw <rvinw@icloud.com>
2026-03-05 06:19:12 +0000

    models for users added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "marketplace",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+AUTH_USER_MODEL = "accounts.User"
