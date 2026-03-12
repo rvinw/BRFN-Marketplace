84d868bb1eb3e5c943ac02edd733ba1542a15c38
Vincent2003-shift <vinnierp03@gmail.com>
2026-03-12 18:11:38 +0000

    changed all the file structure, added new folders, added pages for the functional requirements and allowed transition between pages as well as adding footer

---
+++ frontend/src/components/Pages/CustomerRegisterPage.jsx
@@ -0,0 +1,3 @@
+export default function CustomerRegisterPage() {
+  return <div style={{padding:'2rem'}}>Customer Register — coming soon</div>
+}
---
+++ frontend/src/components/Pages/ProductsPage.jsx
@@ -0,0 +1,3 @@
+export default function ProductsPage() {
+  return <div style={{padding:'2rem'}}>Products — coming soon</div>
+}
