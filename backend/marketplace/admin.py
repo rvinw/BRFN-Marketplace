c7eb51c86bb756e4c238f33fe6b68c846236f272
rvinw <rvinw@icloud.com>
2026-03-23 14:18:17 +0000

    added an access to the admin page

--- backend/marketplace/admin.py
+++ backend/marketplace/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, CartItem, Category, Order, OrderItem, OrderProducer, Product
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderProducer)
+admin.site.register(OrderItem)
